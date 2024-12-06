ddc470e97794a69f577c422f4ce37e0886c0946c
ashraffahim <ashraf.fahim75@gmail.com>
2024-12-06 22:25:23 +0400

    Refactored for routes

--- server.js
+++ server.js
@@ -17,28 +17,35 @@ app.use((req, res, next) => {
 });
 
 app.use((request, response, next) => {
-    console.log(request.url);
-    // response.writeHead(200, { 'Content-Type': 'application/json' });
-    return next();
-});
+    console.log('Serviing request: ' + request.url);
 
-app.param('collection', async (request, response, next, collection) => {
     let db;
 
     MongoClient.connect('mongodb+srv://ashraffahim75:Ashraf12345@atlascluster.v8ty5ip.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
         .then(client => {
             db = client.db('webstore');
             request.db = db;
-            request.collection = db.collection(collection);
             return next();
         });
 });
 
-app.get('/:collection', async (request, response, next) => {
-    response.send(await request.collection.find({}).toArray());
+app.get('/products/:filter?', async (request, response, next) => {
+    let filter = {};
+
+    if (request.params.filter) {
+        filter = {
+            $or: [
+                { "title": new RegExp(`.*${request.params.filter}.*`, 'i') },
+                { "description": new RegExp(`.*${request.params.filter}.*`, 'i') },
+                { "price": new RegExp(`.*${request.params.filter}.*`, 'i') }
+            ]
+        };
+    }
+
+    response.send(await request.db.collection('product').find(filter).toArray());
 });
 
-app.post('/:collection', async (request, response, next) => {
+app.post('/orders', async (request, response, next) => {
     const doc = { ...request.body };
 
     const productQtys = {};
@@ -56,7 +63,7 @@ app.post('/:collection', async (request, response, next) => {
 
     doc.products = productQtys;
 
-    const insertedDoc = await request.collection.insertOne(doc);
+    const insertedDoc = await request.db.collection('order').insertOne(doc);
 
     if (insertedDoc.insertedId) {
         response.send({ success: 1 }).status(200);
@@ -65,13 +72,40 @@ app.post('/:collection', async (request, response, next) => {
     }
 });
 
-app.delete('/:collection/:id', async (request, response, next) => {
-    const { acknowledged, deletedCount } = await request.collection.deleteOne({ _id: new ObjectId(request.params.id) });
+app.put('/orders/:id', async (request, response, next) => {
+    const doc = { ...request.body };
 
-    if (acknowledged) {
-        response.send({ success: 1, deletedCount }).status(200);
+    const productQtys = {};
+
+    doc.products.forEach(product => {
+        if (!productQtys[product]) productQtys[product] = 0;
+
+        productQtys[product]++;
+    });
+
+    for (const key in productQtys) {
+        const { matchedCount, upsertedCount } = await request.db.collection('product').updateOne({ _id: new ObjectId(key) }, { $inc: { inventory: -1 * parseInt(productQtys[key]) } });
+        console.log({ matchedCount, upsertedCount });
+    }
+
+    doc.products = productQtys;
+
+    const insertedDoc = await request.db.collection('order').insertOne(doc);
+
+    if (insertedDoc.insertedId) {
+        response.send({ success: 1 }).status(200);
     } else {
-        response.send({ success: 0, deletedCount }).status(500);
+        response.send({ success: 0 }).status(500);
+    }
+});
+
+app.delete('/orders/:id', async (request, response, next) => {
+    const { acknowledged, deletedCount } = await request.db.collection('order').deleteOne({ _id: new ObjectId(request.params.id) });
+
+    if (deletedCount > 0) {
+        response.send({ success: 1 }).status(200);
+    } else {
+        response.send({ success: 0 }).status(500);
     }
 });
 
