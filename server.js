7ec42edc07993409977350bb766ac2a35d306dc3
ashraffahim <ashraf.fahim75@gmail.com>
2024-12-10 10:32:31 +0400

    Updated, route, log, var
    Added order list for API
    Removed unnecessary variable
    Stock adjustment after order place, alter and delete

--- server.js
+++ server.js
@@ -17,14 +17,12 @@ app.use((req, res, next) => {
 });
 
 app.use((request, response, next) => {
-    console.log('Serviing request: ' + request.url);
-
-    let db;
+    console.log('Serving request: ' + request.method + ' ' + request.url);
 
     MongoClient.connect('mongodb+srv://ashraffahim75:Ashraf12345@atlascluster.v8ty5ip.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
         .then(client => {
-            db = client.db('webstore');
-            request.db = db;
+            request.db = client.db('webstore');
+            
             return next();
         });
 });
@@ -45,6 +43,10 @@ app.get('/products/:filter?', async (request, response, next) => {
     response.send(await request.db.collection('product').find(filter).toArray());
 });
 
+app.get('/orders', async (request, response, next) => {
+    response.send(await request.db.collection('order').find({}).toArray());
+});
+
 app.post('/orders', async (request, response, next) => {
     const doc = { ...request.body };
 
@@ -57,22 +59,36 @@ app.post('/orders', async (request, response, next) => {
     });
 
     for (const key in productQtys) {
-        const { matchedCount, upsertedCount } = await request.db.collection('product').updateOne({ _id: new ObjectId(key) }, { $inc: { inventory: -1 * parseInt(productQtys[key]) } });
-        console.log({ matchedCount, upsertedCount });
+        const { matchedCount, upsertedCount } = await request.db.collection('product').updateOne({ _id: new ObjectId(key), place: { "$gte": parseInt(productQtys[key]) } }, { $inc: { place: -1 * parseInt(productQtys[key]) } });
+
+        if (matchedCount === 0) {
+            console.log('Activity is not available: ' + key);
+
+            delete productQtys[key];
+        }
     }
 
-    doc.products = productQtys;
+    if (Object.keys(productQtys).length) {
+        doc.products = productQtys;
+        const insertedDoc = await request.db.collection('order').insertOne(doc);
 
-    const insertedDoc = await request.db.collection('order').insertOne(doc);
-
-    if (insertedDoc.insertedId) {
-        response.send({ success: 1 }).status(200);
-    } else {
-        response.send({ success: 0 }).status(500);
+        if (insertedDoc.insertedId) {
+            return response.send({ success: 1 }).status(200);
+        }
     }
+
+    response.send({ success: 0 }).status(500);
+
 });
 
 app.put('/orders/:id', async (request, response, next) => {
+
+    const order = await request.db.collection('order').findOne({ _id: new ObjectId(request.params.id) });
+
+    if (order === null) {
+        return response.status(404).send({ message: 'Order not found' });
+    }
+
     const doc = { ...request.body };
 
     const productQtys = {};
@@ -83,16 +99,19 @@ app.put('/orders/:id', async (request, response, next) => {
         productQtys[product]++;
     });
 
-    for (const key in productQtys) {
-        const { matchedCount, upsertedCount } = await request.db.collection('product').updateOne({ _id: new ObjectId(key) }, { $inc: { inventory: -1 * parseInt(productQtys[key]) } });
-        console.log({ matchedCount, upsertedCount });
-    }
+    Object.entries(order.products).forEach(async ([id, qty]) => {
+        const { productMatchedCount, productUpsertedCount } = await request.db.collection('product').updateOne({ _id: new ObjectId(id), place: { "$gte": (parseInt(qty) - (productQtys[id] ?? 0 )) } }, { $inc: { place: (parseInt(qty) - (productQtys[id] ?? 0 )) } });
 
-    doc.products = productQtys;
+        if (productMatchedCount === 0) {
+            console.log('Activity is not available: ' + id);
 
-    const insertedDoc = await request.db.collection('order').insertOne(doc);
+            delete productQtys[id];
+        }
+    });
+    
+    const { orderMatchedCount, orderUpsertedCount } = await request.db.collection('order').updateOne({ _id: new ObjectId(request.params.id) }, { $set: { products: productQtys } });
 
-    if (insertedDoc.insertedId) {
+    if (orderMatchedCount > 0) {
         response.send({ success: 1 }).status(200);
     } else {
         response.send({ success: 0 }).status(500);
